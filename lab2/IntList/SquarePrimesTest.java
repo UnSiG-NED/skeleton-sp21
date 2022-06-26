b21c7de9501fff4baebbe08c28a33ad5ae876708
Haoyu Zhang <haoyu_z@outlook.com>
2022-06-26 16:59:32 -0400

    dev: finish lab2

--- lab2/IntList/SquarePrimesTest.java
+++ lab2/IntList/SquarePrimesTest.java
@@ -17,4 +17,20 @@ public class SquarePrimesTest {
         assertEquals("14 -> 15 -> 16 -> 289 -> 18", lst.toString());
         assertTrue(changed);
     }
+
+    @Test
+    public void testSP1() {
+        IntList lst = IntList.of(0, 1, 2, 3, 4, 7);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("0 -> 1 -> 4 -> 9 -> 4 -> 49", lst.toString());
+        assertTrue(changed);
+    }
+
+    @Test
+    public void testSP2() {
+        IntList lst = IntList.of(2, 3);
+        boolean changed = IntListExercises.squarePrimes(lst);
+        assertEquals("4 -> 9", lst.toString());
+        assertTrue(changed);
+    }
 }
